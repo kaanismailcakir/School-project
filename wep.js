da0fd1969a817b0ef524f847ef3891abf4180bc9
kaanismailcakir <85019889+kaanismailcakir@users.noreply.github.com>
2021-10-24 14:05:54 +0300

    okul projesi

---
+++ wep.js
@@ -0,0 +1,10 @@
+document.querySelector("button")
+   .addEventListener("click" , () => 
+   {
+       alert("Başarıyla gönderildi")
+   })
+
+document.querySelector("button1")
+    .addEventListener("click",() => {
+        alert("Yeni Sekme Açılıyor")
+    })
